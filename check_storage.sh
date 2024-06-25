192b78b6c2d4d32428685773be0a83392d0e9889
Catalin Tudorache <karatheem@gmail.com>
2024-06-25 17:46:22 +0300

    Creating storage checker script.

--- check_storage.sh
+++ check_storage.sh
@@ -5,4 +5,4 @@ echo "--------------------------------"
 df -h | awk '{ if (NR==1) print $0; else if ($5 > 80) print $0 }'
 echo "--------------------------------"
 echo "Filesystem over 80% occupied:"
-df - h | awk '$5 > 80' | wc -l
+df -h | awk '$5 > 80' | wc -l
