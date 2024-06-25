8c7803c0fa18111728007a2963abe1a89cb7a767
Catalin Tudorache <karatheem@gmail.com>
2024-06-25 18:36:29 +0300

    Adding print to identify errors

--- check_storage.sh
+++ check_storage.sh
@@ -2,7 +2,7 @@
 
 echo "Checking storage for $(uname -n)"
 echo "--------------------------------"
-df -h | awk '{ if (NR==1) print $0; else if ($5 > 80) print $0 }'
+df -h | awk '{ if (NR==1) print $0; else if ($5 + 0 > 80) print $0 }'
 echo "--------------------------------"
 echo "Filesystems over 80% occupied:"
-df -h | awk '$5 > 80' | wc -l
+df -h | awk '$5 + 0 > 80 {print $0}' 
