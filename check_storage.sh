aa61ac11ae577b80567d0d4a59b1433303db3e85
Catalin Tudorache <karatheem@gmail.com>
2024-06-25 17:48:50 +0300

    Updating script

--- check_storage.sh
+++ check_storage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Checking storage for ($hostname)"
+echo "Checking storage for $(hostname)"
 echo "--------------------------------"
 df -h | awk '{ if (NR==1) print $0; else if ($5 > 80) print $0 }'
 echo "--------------------------------"
-echo "Filesystem over 80% occupied:"
+echo "Filesystems over 80% occupied:"
 df -h | awk '$5 > 80' | wc -l
