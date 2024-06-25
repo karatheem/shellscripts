141892ff1a020062b783d8b0e9e9bd9de009b340
Catalin Tudorache <karatheem@gmail.com>
2024-06-25 17:55:13 +0300

    updating script

--- check_storage.sh
+++ check_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Checking storage for $(hostname)"
+echo "Checking storage for $(uname -n)"
 echo "--------------------------------"
 df -h | awk '{ if (NR==1) print $0; else if ($5 > 80) print $0 }'
 echo "--------------------------------"
